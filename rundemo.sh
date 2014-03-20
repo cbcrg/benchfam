1670a0455dd3646e6767cfeef36c068206745838
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-03-20 11:30:28 +0100

    Added star args for batch

--- rundemo.sh
+++ rundemo.sh
@@ -1,3 +1,3 @@
 CFG=''; if [ -f cluster.config ]; then CFG='-c cluster.config'; fi
-./nextflow $CFG pfam3d.nf --limit 50 --blast-db tutorial/db_50/blast/pdb_small --db-cache tutorial/db_50/
+./nextflow $CFG pfam3d.nf --limit 50 --blast-db tutorial/db_50/blast/pdb_small --db-cache tutorial/db_50/ "$@"
 
